0a039e0bdb5a4f09cd6f146a69b3f623dae6cba3
nituacor <nituacor@3ee79ce7-82ee-0310-b345-eaab264f38a4>
2011-01-26 12:51:36 +0000

    add dependencies
    git-svn-id: https://svn.eionet.europa.eu/repositories/GBIF/trunk/Products.PyDigirSearch@742 3ee79ce7-82ee-0310-b345-eaab264f38a4

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(name='Products.PyDigirSearch',
     url='http://naaya.eaudeweb.ro',
     packages=find_packages(),
     include_package_data=True,
-    zip_safe=False
+    zip_safe=False,
+    install_requires=['lxml']
 )
