a7daacf7721bb1a6512da6c52fd029e77fcc8cce
plugaale <plugaale@3ee79ce7-82ee-0310-b345-eaab264f38a4>
2011-02-24 13:48:15 +0000

    On start fill all the select items
    git-svn-id: https://svn.eionet.europa.eu/repositories/GBIF/trunk/Products.PyDigirSearch@784 3ee79ce7-82ee-0310-b345-eaab264f38a4

--- Products/PyDigirSearch/www/js/search_digir.js
+++ Products/PyDigirSearch/www/js/search_digir.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-
+/**
+ * A new Jquery ui widget using jquery autocomplete.
+*/
 $.widget("ui.combobox", {
 	options: {
 		source: function(request, response){
@@ -9,7 +11,8 @@ $.widget("ui.combobox", {
 			ui.item.option.selected = true;
 			//Set value to the select
 			var select = $('#' + $(this).attr('target'));
-			if(select.find("[value="+ ui.item.value +"]")){
+			// Don't add this option if it's already in select
+			if(select.find("[value="+ ui.item.value +"]").size() == 0){
 				select.append($("<option>").val(ui.item.value));
 			}
 			select.val(ui.item.value).trigger("change");
@@ -33,6 +36,12 @@ $.widget("ui.combobox", {
 					// remove invalid value, as it didn't match anything
 					$(this).val("");
 					select.val("");
+					if (self.attr('target') == 'Family'){
+						genus_el.empty();
+					}
+					if (self.attr('target') == 'Genus'){
+						species_el.empty();
+					}
 					return false;
 				}
 			}
@@ -99,6 +108,7 @@ $.widget("ui.combobox", {
 	}
 });
 
+//Future comboboxes
 var family_el = $('#Family');
 var genus_el = $('#Genus');
 var species_el = $('#Species');
@@ -109,6 +119,145 @@ var collection_el = $('#CollectionCode');
 var basisofrecord_el = $('#BasisOfRecord');
 var country_el = $('#Country');
 
+//If the value of family is already set then get all genuses for that specific family
+if(family_el.val() !== ''){
+	$.getJSON('get_json', {'type': 'genus_by_family',
+				  'value': family_el.val()}, function(data){
+		$.each(data, function(){
+			var val = this.Genus;
+			// Don't add this option if it's already in select
+			if (genus_el.find("[value="+ val +"]").size() == 0){
+				genus_el.append($("<option>").val(val).text(val));
+			}
+		});
+	});
+}
+
+family_el.combobox({
+	source: function(request, response){
+		handle_source('families', request, response);
+	},
+	select: function(event, ui){
+		ui.item.option.selected = true;
+
+		//Set value to the select
+		var select = $('#' + $(this).attr('target'));
+		if(select.find("[value="+ ui.item.value+"]")){
+			select.append($("<option>").val(ui.item.value));
+		}
+		select.val(ui.item.value).trigger("change");
+
+		genus_el.empty();
+		species_el.empty();
+
+		$.getJSON('get_json', {'type': 'genus_by_family',
+				  'value': ui.item.value}, function(data){
+			$.each(data, function(){
+				var val = this.Genus;
+				if (genus_el.find("[val="+ val +"]").size() == 0){
+					genus_el.append($("<option>").val(val).text(val));
+				}
+			});
+			//Add an empty option so that the request does not set the species automatically
+			genus_el.append($("<option>").val("").text("All"));
+			genus_el.val("");
+		});
+		return;
+	}
+});
+
+//If the value of genus is already set then get all species for that specific genus
+if(genus_el.val() !== ''){
+	$.getJSON('get_json', {'type': 'species_by_genus',
+				  'value': genus_el.val()}, function(data){
+		$.each(data, function(){
+			var val = this.Species;
+			// Don't add this option if it's already in select
+			if (species_el.find("[value="+ val +"]").size() == 0){
+				species_el.append($("<option>").val(val).text(val));
+			}
+		});
+	});
+}
+
+genus_el.combobox({
+	source: function(request, response){
+		if (genus_el.children("option").length > 0){
+			options_source(this, request, response);
+		}else{
+			handle_source('genus', request, response);
+		}
+	},
+	select: function(event, ui){
+		ui.item.option.selected = true;
+		species_el.empty();
+		$.getJSON('get_json', {'type': 'species_by_genus', 'value': ui.item.value},
+				  function(data){
+			$.each(data, function(){
+				var val = this.Species;
+				if (species_el.find("[value="+ val +"]").size() == 0){
+					species_el.append($("<option>").val(val).text(val));
+				}
+			});
+			//Add an empty option so that the request does not set the species automatically
+			species_el.append($("<option>").val("").text("All"));
+			species_el.val("");
+		});
+		return;
+	}
+});
+
+species_el.combobox({
+	source: function(request, response){
+		if (species_el.children("option").length > 0){
+			options_source(this, request, response);
+		}else{
+			handle_source('species', request, response);
+		}
+	},
+});
+
+institution_el.combobox({
+	source: function(request, response){
+		handle_source('institutions', request, response);
+	}
+});
+
+collection_el.combobox({
+	source: function(request, response){
+		handle_source('collections', request, response);
+	}
+});
+
+basisofrecord_el.combobox({
+	source: function(request, response){
+		handle_source('basisofrecords', request, response);
+	}
+});
+
+names_el.combobox({
+	source: function(request, response){
+		handle_source('names', request, response);
+	},
+});
+
+country_el.combobox({
+	source: function(request, response){
+		handle_source('countries', request, response);
+	},
+});
+
+locality_el.combobox({
+	source: function(request, response){
+		handle_source('localities', request, response);
+	},
+});
+
+
+$("#reset-button").click(function(){
+	$("select").empty();
+});
+});
 
 function options_source(self, request, response){
 	var select = $('#' + self.element.attr('target'));
@@ -143,107 +292,3 @@ function handle_source(type, request, response){
 		}));
 	});
 }
-
-institution_el.combobox({
-	source: function(request, response){
-		handle_source('institutions', request, response);
-	}
-});
-
-collection_el.combobox({
-	source: function(request, response){
-		handle_source('collections', request, response);
-	}
-});
-
-basisofrecord_el.combobox({
-	source: function(request, response){
-		handle_source('basisofrecords', request, response);
-	}
-});
-
-family_el.combobox({
-	source: function(request, response){
-		handle_source('families', request, response);
-	},
-	select: function(event, ui){
-		ui.item.option.selected = true;
-
-		//Set value to the select
-		var select = $('#' + $(this).attr('target'));
-		if(select.find("[value="+ ui.item.value+"]")){
-			select.append($("<option>").val(ui.item.value));
-		}
-		select.val(ui.item.value).trigger("change");
-
-		genus_el.empty();
-		species_el.empty();
-
-		$.getJSON('get_json', {'type': 'genus_by_family',
-				  'value': ui.item.value}, function(data){
-			$.each(data, function(){
-				var val = this.Genus;
-				var new_option = $("<option>").val(val).text(val);
-				genus_el.append(new_option);
-			});
-		});
-		return;
-	}
-});
-
-genus_el.combobox({
-	source: function(request, response){
-		if (genus_el.children("option").length > 0){
-			options_source(this, request, response);
-		}else{
-			handle_source('genus', request, response);
-		}
-	},
-	select: function(event, ui){
-		ui.item.option.selected = true;
-		species_el.empty();
-		$.getJSON('get_json', {'type': 'species_by_genus', 'value': ui.item.value},
-				  function(data){
-			$.each(data, function(){
-				var val = this.Species;
-				var new_option = $("<option>").val(val).text(val);
-				species_el.append(new_option);
-			});
-		});
-		return;
-	}
-});
-
-species_el.combobox({
-	source: function(request, response){
-		if (species_el.children("option").length > 0){
-			options_source(this, request, response);
-		}else{
-			handle_source('species', request, response);
-		}
-	},
-});
-
-names_el.combobox({
-	source: function(request, response){
-		handle_source('names', request, response);
-	},
-});
-
-country_el.combobox({
-	source: function(request, response){
-		handle_source('countries', request, response);
-	},
-});
-
-locality_el.combobox({
-	source: function(request, response){
-		handle_source('localities', request, response);
-	},
-});
-
-
-$("#reset-button").click(function(){
-	$("select").empty();
-});
-});
